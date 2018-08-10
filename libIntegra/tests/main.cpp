ec709158b0170270bc300a22197cc1ef5b7816af
Jamie Bullock <jamie@jamiebullock.com>
2018-08-10 11:41:51 +0100

    Add initial startup test

--- libIntegra/tests/main.cpp
+++ libIntegra/tests/main.cpp
@@ -6,15 +6,56 @@
 ////  Copyright © 2018 Birmingham Conservatoire. All rights reserved.
 ////
 
+#include "server_startup_info.h"
+#include "integra_session.h"
+#include "interface_definition.h"
+#include "error.h"
+#include "server.h"
+#include "trace.h"
+#include "server_lock.h"
+#include "command.h"
+#include "path.h"
 
 #include "gtest.h"
 
 using namespace testing;
+using namespace integra_api;
 
-TEST(CaseName, TestName)
+namespace k
 {
-    ASSERT_EQ(1, 0) << "Hello world";
+    const std::string moduleDirectory           = "Integra.framework/Resources/modules";
+    const std::string thirdPartyModuleDirectory = "third_party";
 }
+
+class IntegraTest : public ::testing::Test
+{
+protected:
+    
+    IntegraTest()
+    {
+        sinfo.system_module_directory       = k::moduleDirectory;
+        sinfo.third_party_module_directory  = k::thirdPartyModuleDirectory;
+        CTrace::set_categories_to_trace(false, false, false);
+    }
+    
+    ~IntegraTest() override {}
+    
+    void SetUp() override {}
+    void TearDown() override {}
+    
+    CServerStartupInfo sinfo;
+};
+
+// Test server startup
+TEST_F(IntegraTest, SessionStartsCorrectly)
+{
+    CIntegraSession session;
+    CError err = session.start_session(sinfo);
+    ASSERT_EQ(err, CError::code::SUCCESS);
+}
+
+// Test server shutdown
+
 
 int main(int argc, char **argv)
 {
